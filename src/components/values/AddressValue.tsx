254fc33252653055995b58eaf748abd6eb32191e
totomanov <totomanov@protonmail.com>
2024-11-29 14:57:56 +0700

    prettify addresses

--- src/components/values/AddressValue.tsx
+++ src/components/values/AddressValue.tsx
@@ -14,11 +14,25 @@ function AddressValue({ a }: Props) {
 
   const bits = keccak256(a).slice(2);
   const h = Number(`0x${bits.slice(0, 8)}`) % 360;
-  const s = (Number(`0x${bits.slice(8, 16)}`) % 60) + 40;
-  const l = (Number(`0x${bits.slice(16, 24)}`) % 30) + 20;
+  const s = (Number(`0x${bits.slice(8, 16)}`) % 50) + 50;
+  const l = (Number(`0x${bits.slice(16, 24)}`) % 30) + 65;
   const color = `hsl(${h} ${s}% ${l}%)`;
 
   const shortAddress = `${a.substring(0, 6)}...${a.substring(36)}`;
+
+  const content = metadata
+    ? (() => {
+        if (metadata.kind === "vault") {
+          return `${metadata.name}`;
+        } else if (metadata.kind === "oracle") {
+          return <span>{metadata.name}</span>;
+        } else if (metadata.kind === "token") {
+          return <span>{metadata.symbol}</span>;
+        } else {
+          return <span>{metadata.label}</span>;
+        }
+      })()
+    : shortAddress;
 
   return (
     <Link
@@ -27,23 +41,16 @@ function AddressValue({ a }: Props) {
       whiteSpace="nowrap"
       isExternal
     >
-      {metadata ? (
-        <Box as="span" color={color} fontWeight="bold">
-          {(() => {
-            if (metadata.kind === "vault") {
-              return <span>{metadata.name}</span>;
-            } else if (metadata.kind === "oracle") {
-              return <span>{metadata.name}</span>;
-            } else if (metadata.kind === "token") {
-              return <span>{metadata.symbol}</span>;
-            } else {
-              return <span>{metadata.label}</span>;
-            }
-          })()}
-        </Box>
-      ) : (
-        shortAddress
-      )}
+      <Box
+        as="span"
+        bgColor={color}
+        color="black"
+        px={1}
+        rounded="full"
+        fontWeight="semibold"
+      >
+        {content}
+      </Box>
     </Link>
   );
 }
